7d64f98bb8e6e4c47e2a4b90cc750d64ca2de52f
Chris Madrigal <inzanitygaminggod@gmail.com>
2023-12-13 21:49:37 -0500

    Doesn't allow users to make a Quest before the current local date

--- Completionist/app/src/main/java/com/example/completionist/TaskPage/AddNewTaskPage.kt
+++ Completionist/app/src/main/java/com/example/completionist/TaskPage/AddNewTaskPage.kt
@@ -1,15 +1,19 @@
 package com.example.completionist.TaskPage
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.completionist.R
 import com.google.firebase.auth.FirebaseAuth
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class AddNewTaskPage : AppCompatActivity() {
@@ -33,10 +37,20 @@ class AddNewTaskPage : AppCompatActivity() {
             val questPointsText = questPoints.text.toString().toInt()
 
             // Check if quest_date is empty
-            val questDateText = if (questDate.text.toString().isNotEmpty()) {
-                questDate.text.toString()
+            val questDateText = questDate.text.toString()
+
+            // Validate the date
+            if (questDateText.isNotEmpty() && !isValidDate(questDateText)) {
+                // Show an error message or handle the invalid date case
+                // For example, you can show a Toast message
+                showCustomToast(this, "Please enter a valid date")
+                return@setOnClickListener
+            }
+
+            // If quest_date is empty, use the current selected date as default
+            val finalQuestDate = if (questDateText.isNotEmpty()) {
+                questDateText
             } else {
-                // Use the current selected date as default
                 intent.getStringExtra("CURRENT_DATE")
             }
 
@@ -46,7 +60,7 @@ class AddNewTaskPage : AppCompatActivity() {
             val resultIntent = Intent()
             resultIntent.putExtra("QUEST_NAME", questNameText)
             resultIntent.putExtra("QUEST_POINTS", questPointsText)
-            resultIntent.putExtra("QUEST_DATE", questDateText)
+            resultIntent.putExtra("QUEST_DATE", finalQuestDate)
             resultIntent.putExtra("USER_ID", userId)
 
             setResult(Activity.RESULT_OK, resultIntent)
@@ -65,4 +79,24 @@ class AddNewTaskPage : AppCompatActivity() {
         val firebaseUser = FirebaseAuth.getInstance().currentUser
         return firebaseUser?.uid ?: ""
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun isValidDate(dateText: String): Boolean {
+        val currentDate = LocalDate.now()
+        val enteredDate = LocalDate.parse(dateText, DateTimeFormatter.ofPattern("MM/dd/yyyy"))
+        return !enteredDate.isBefore(currentDate)
+    }
+
+    fun showCustomToast(context: Context, message: String) {
+        val builder = AlertDialog.Builder(context)
+        builder.setMessage(message)
+        builder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
 }
+
+
